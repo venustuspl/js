b222c4dc3fe94a6d7b21d31abe04740d23d5820d
venustuspl <venustus.pl@gmail.com>
2021-10-09 21:38:34 +0200

    package commit

---
+++ src/test/java/pl/venustus/JS/JsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.venustus.JS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
