f4c2ecc481f0af2b12ee76e60f4af8f46477e667
venustuspl <venustus.pl@gmail.com>
2021-11-23 21:42:38 +0100

    33 - bacground color

---
+++ src/main/resources/static/33 - background color/main.js
@@ -0,0 +1,5 @@
+// KOLOR GENEROWANY W ZALEŻNOŚCI OD TEGO GDZIE KLIKNIEMY
+
+// jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
+// jesli wartość dla x i y jest nieparzysta to kolor niebieski
+// jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
