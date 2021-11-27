39e2e4acbbfe8d710d46c1b6fe3f32ce8445c936
Tomek K <venustus.pl@gmai.com>
2021-11-27 14:04:26 +0100

    35 - screen dimming v2

--- src/main/resources/static/35 - screen dimming/main.js
+++ src/main/resources/static/35 - screen dimming/main.js
@@ -1,5 +1,3 @@
-/* Gotowy projekt - sprzawdz działanie strzałek i zrób podobnie. http://websamuraj.pl/examples/js/projekt4-zad2/ */
-
 let red = 100;
 let green = 100;
 let blue = 100;
@@ -7,14 +5,36 @@ let blue = 100;
 document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 
 const changeColor = (e) => {
-  console.log(e.keyCode, e.which);
+  // console.log(e.keyCode, e.which);
   // 38 - strzałka do góry
   // 40 - strzalka w dół
 
-  // wciśnięcie klawisza strzałki do góry ma zmieniać kolor (stopniowo) na biały czyli rgb(255,255,255). Wciśniecie klawisza strzałki w dół ma zmienić kolor stopniowo na czarny (docelowo rgb(0,0,0)) 
-  //tutaj twój kod
-  // wersja 1 - instrukcja if
+  // INSTRUKCJA IF
+  // if (e.keyCode === 38 && red < 255) {
+
+  //   red += 1;
+  //   green += 1;
+  //   blue += 1;
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // } else if (e.keyCode === 40 && red >= 0) {
+  //   red--;
+  //   green--;
+  //   blue--;
+  //   document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  // }
+
+
   // wersja 2 - instrukcja switch
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red < 255 ? ++red : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? ++blue : blue})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red > 0 ? --red : red}, ${green > 0 ? --green : green}, ${blue > 0 ? --blue : blue})`;
+      break;
+  }
+  console.log(red);
 
 }
 
