9d3b8dc54e319f908273fdd35362639c85fd04c1
Tomek K <venustus.pl@gmai.com>
2021-11-29 18:29:57 +0100

    36 - password input

---
+++ src/main/resources/static/36 - input/main.js
@@ -0,0 +1,22 @@
+const input = document.getElementById('pass');
+const div = document.querySelector('.message');
+const password = "user";
+const message = "wyjechałam na zawsze"
+
+input.addEventListener('input', (e) => {
+ // console.log(e.target.value);
+ if (password === e.target.value) {
+  div.textContent = message;
+  e.target.value = '';
+ } else {
+  div.textContent = '';
+ }
+})
+
+input.addEventListener('focus', (e) => {
+ e.target.classList.add('active');
+})
+
+input.addEventListener('blur', (e) => {
+ e.target.classList.remove('active');
+})
