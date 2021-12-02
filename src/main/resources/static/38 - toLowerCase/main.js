c35c0734222f0e19908a65f32f0778bd96ddf064
venustuspl <venustus.pl@gmail.com>
2021-12-02 20:42:18 +0100

    38 - toLowerCase v2

--- src/main/resources/static/38 - toLowerCase/main.js
+++ src/main/resources/static/38 - toLowerCase/main.js
@@ -1,14 +1,13 @@
-// Projekt 5 - Zadanie dla Ciebie 
-// Sprawdź czy wpisane w input hasło pasuje do któegoś z hasel w tablicy. Jeśli tak wyświetl wiadomość w <div>
-//wielkość liter nie ma znaczenia (wykorzystaj metodę .toUpperCase lub .toLowerCase - pamiętaj, że metody te działają na pojedyńczym elemencie typu string)
-
 const input = document.querySelector("input");
 const passwords = ['jedEN', 'DwA'];
 const messages = ["super", "działa!"]
 
 const showMessage = (e) => {
- //tutaj rozwiązanie
- // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
+ passwords.forEach((item, i) => {
+  if (item.toLowerCase() === e.target.value.toLowerCase()) {
+   document.querySelector('div').textContent = messages[i];
+  }
+ })
 }
 
 input.addEventListener("input", showMessage)
