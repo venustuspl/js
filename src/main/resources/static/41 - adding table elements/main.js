9a15a4537766f4a1c44a4a2376afda1b8a4b2ab2
venustuspl <venustus.pl@gmail.com>
2021-12-08 18:29:56 +0100

    40 - code generator

---
+++ src/main/resources/static/41 - adding table elements/main.js
@@ -0,0 +1,22 @@
+const names = [];
+const div = document.querySelector('div')
+
+const addName = (e) => {
+ e.preventDefault();
+ const input = document.querySelector('input');
+ const newName = input.value;
+ if (input.value.length) {
+  for (name of names) {
+   if (name === newName) {
+    alert("to już jest!!!!!!");
+    return
+   }
+  }
+  names.push(newName);
+  // console.log(names);
+  div.textContent += newName + ", ";
+  input.value = "";
+ }
+
+}
+document.querySelector('button').addEventListener('click', addName)
