211ed45938c3d7c2bcf0c8d8c1aeb953b3ce03dd
Tomek K <venustus.pl@gmai.com>
2021-12-18 09:32:48 +0100

    48 - setInterval

---
+++ src/main/resources/static/48 - setInterval/main.js
@@ -0,0 +1,14 @@
+const spnText = document.querySelector('.text');
+const txt = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore tenetur rerum ad? Eum quos reprehenderit minima! Fugit, omnis! Eveniet repellendus et iure deserunt iusto quas in est odit, labore optio? KONIEC'
+
+// Parametry
+let indexText = 0;
+const time = 40;
+
+// Implementacja
+const addLetter = () => {
+ spnText.textContent += txt[indexText];
+ indexText++;
+ if (indexText === txt.length) clearInterval(indexTyping);
+}
+const indexTyping = setInterval(addLetter, time);
