7dd0b95e62d4ade6e238fe08facdb96f81bb4e34
venustuspl <venustus.pl@gmail.com>
2022-01-06 08:49:11 +0100

    57 - ul li list

---
+++ src/main/resources/static/58 - ul li/zadanie1-delete-projekt/main.js
@@ -0,0 +1,12 @@
+const removeTask = (e) => {
+ // e.target.remove();
+ // e.target.parentNode.remove();
+ // console.log(e.target.parentNode);
+ // e.target.parentNode.style.textDecoration = "line-through";
+ // e.target.remove();
+ const index = e.target.dataset.key;
+ document.querySelector(`li[data-key="${index}"]`).remove()
+
+}
+
+document.querySelectorAll('button[data-key]').forEach(item => item.addEventListener('click', removeTask))
