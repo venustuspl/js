962d567a83ae5bf07ced4c5bead3db5a215cb5f7
venustuspl <venustus.pl@gmail.com>
2022-01-07 06:55:52 +0100

    58 - map,

---
+++ src/main/resources/static/58 - map, filter/main.js
@@ -0,0 +1,17 @@
+// FILTER (I PRZYPOMNIENIE MAP)
+
+// Filter
+const arr = [34, 219, 109, 2934, 12, 10, 29];
+
+const oddNumbers = arr.filter(number => number % 2);
+const evenNumbers = arr.filter(number => !(number % 2));
+const numbersBiggerThen100 = arr.filter(number => number > 100);
+
+// Map
+const double = arr.map(number => number * 2);
+const people = arr.map(number => number + " osób");
+
+//forEach
+
+arr.forEach((number, index) => arr[index] = number * 2)
+
